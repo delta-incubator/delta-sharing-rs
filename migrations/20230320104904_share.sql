2d95e6b4c88a433082cd41d6cfb287ecdc7c2906
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2023-03-20 19:52:31 +0900

    Add share table

---
+++ migrations/20230320104904_share.sql
@@ -0,0 +1,8 @@
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS share (
+    id UUID PRIMARY KEY,
+    name VARCHAR NOT NULL UNIQUE,
+    created_by UUID NOT NULL REFERENCES account(id),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL default CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL default CURRENT_TIMESTAMP
+);
