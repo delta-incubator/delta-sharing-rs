8fe39557d504085b49e5417741e4f39c21924d43
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2023-03-24 21:18:17 +0900

    Implement JWT auth middleware

--- src/utils/jwt.rs
+++ src/utils/jwt.rs
@@ -1,23 +1,29 @@
 use crate::config::JWT_SECRET;
 use crate::error::Error;
+use crate::server::entities::account::Entity as AccountEntity;
+use crate::server::entities::account::Name as AccountName;
+use crate::server::interactors::SharedState;
+use anyhow::anyhow;
+use anyhow::Context;
+use anyhow::Result;
 use axum::async_trait;
 use axum::extract::FromRequestParts;
 use axum::headers::authorization::Bearer;
 use axum::headers::Authorization;
+use axum::headers::HeaderMapExt;
 use axum::http::request::Parts;
+use axum::http::Request;
+use axum::middleware::Next;
+use axum::response::Response;
 use axum::RequestPartsExt;
 use axum::TypedHeader;
+use chrono::DateTime;
+use chrono::NaiveDateTime;
+use chrono::Utc;
 use jsonwebtoken::decode;
 use jsonwebtoken::DecodingKey;
 use jsonwebtoken::EncodingKey;
 use jsonwebtoken::Validation;
-use std::str::FromStr;
-
-use anyhow::Context;
-use anyhow::Result;
-use chrono::DateTime;
-use chrono::NaiveDateTime;
-use chrono::Utc;
 use std::time::Duration;
 use std::time::SystemTime;
 use std::time::UNIX_EPOCH;
@@ -61,24 +67,15 @@ pub fn expires_in(ttl: i64) -> Result<(i64, DateTime<Utc>)> {
     let ttl = u64::try_from(ttl).context("failed to convert i64 ttl to u64")?;
     let now = SystemTime::now()
         .duration_since(UNIX_EPOCH)
-        .context("failed to create JWT token expiry")?;
-    let expiry = now + Duration::from_secs(ttl);
-    let expiry = expiry.as_millis();
-    let expiry = i64::try_from(expiry).context("failed to convert u128 expiry to i64")?;
-    let expiration_time = NaiveDateTime::from_timestamp_millis(expiry)
-        .context("faield to parse expiry millis to datetime")?;
+        .context("failed to create current system time")?;
+    let expiration_secs = now + Duration::from_secs(ttl);
+    let expiration_secs = expiration_secs.as_secs();
+    let expiration_secs = i64::try_from(expiration_secs)
+        .context("failed to convert u128 expiration seconds to i64")?;
+    let expiration_time = NaiveDateTime::from_timestamp_opt(expiration_secs, 0)
+        .context("faield to parse expiration seconds to datetime")?;
     let expiration_time = DateTime::<Utc>::from_utc(expiration_time, Utc);
-    Ok((expiry, expiration_time))
-}
-
-pub fn required_role_of(path: &str) -> Role {
-    let path = path.trim_start_matches("/");
-    let path = path.split("/").next().unwrap_or("");
-    if let Ok(role) = Role::from_str(path) {
-        role
-    } else {
-        Role::Guest
-    }
+    Ok((expiration_secs, expiration_time))
 }
 
 #[async_trait]
@@ -101,18 +98,56 @@ where
                 let jwt =
                     decode::<Claims>(bearer.token(), &JWT_SECRET.decoding, &Validation::default())
                         .map_err(|_| Error::Unauthorized)?;
-                let required_role = required_role_of(&parts.uri.path());
-                if required_role == Role::Guest {
-                    return Ok(jwt.claims);
-                }
-                if jwt.claims.role == Role::Guest {
-                    return Err(Error::Unauthorized);
-                }
                 return Ok(jwt.claims);
             }
             _ => Err(Error::Unauthorized),
         }
     }
+}
+
+pub async fn as_admin<T>(
+    mut request: Request<T>,
+    next: Next<T>,
+) -> std::result::Result<Response, Error> {
+    let token = request
+        .headers()
+        .typed_get::<Authorization<Bearer>>()
+        .ok_or(Error::BadRequest)?
+        .token()
+        .to_owned();
+    let jwt = decode::<Claims>(&token, &JWT_SECRET.decoding, &Validation::default())
+        .map_err(|_| Error::Unauthorized)?;
+    let state = request
+        .extensions()
+        .get::<SharedState>()
+        .ok_or(anyhow!("failed to acquire shared state"))?;
+    let name = AccountName::new(jwt.claims.name).map_err(|_| Error::ValidationFailed)?;
+    let account = AccountEntity::find_by_name(&name, &state.pg_pool)
+        .await
+        .map_err(|_| anyhow!("error occured while selecting account from database"))?;
+    let Some(account) = account else {
+	return Err(Error::Unauthorized);
+    };
+    if jwt.claims.role != Role::Admin {
+        return Err(Error::Unauthorized);
+    }
+    request.extensions_mut().insert(account);
+    Ok(next.run(request).await)
+}
+
+pub async fn as_guest<T>(
+    request: Request<T>,
+    next: Next<T>,
+) -> std::result::Result<Response, Error> {
+    let token = request
+        .headers()
+        .typed_get::<Authorization<Bearer>>()
+        .ok_or(Error::BadRequest)?
+        .token()
+        .to_owned();
+    decode::<Claims>(&token, &JWT_SECRET.decoding, &Validation::default())
+        .map_err(|_| Error::Unauthorized)?;
+    Ok(next.run(request).await)
 }
 
 #[cfg(test)]
