b5d897481f4c79858af4da1d96bfb98db843ddc7
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2023-03-25 14:58:34 +0900

    Update schema

--- migrations/20230321084901_schema.sql
+++ migrations/20230321084901_schema.sql
@@ -1,10 +1,11 @@
 -- Add migration script here
 CREATE TABLE IF NOT EXISTS "schema" (
     id UUID PRIMARY KEY,
-    name VARCHAR NOT NULL UNIQUE,
-    table_id UUID REFERENCES "table"(id),
-    share_id UUID REFERENCES share(id),
+    name VARCHAR NOT NULL,
+    share_id UUID NOT NULL REFERENCES share(id),
+    table_id UUID NOT NULL REFERENCES "table"(id),
     created_by UUID NOT NULL REFERENCES account(id),
     created_at TIMESTAMP WITH TIME ZONE NOT NULL default CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL default CURRENT_TIMESTAMP
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL default CURRENT_TIMESTAMP,
+    UNIQUE (share_id, name) INCLUDE (id)
 );
