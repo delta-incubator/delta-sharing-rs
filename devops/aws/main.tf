f66e12dcd4a0532167361d64c141a518d5088254
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2023-03-30 14:30:19 +0900

    Add AWS terraform files

---
+++ devops/aws/main.tf
@@ -0,0 +1,26 @@
+//variable "project" {}
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+variable "bucket" {}
+variable "acl" {}
+
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
+}
+
+resource "aws_s3_bucket" "kotosiro_sharing_s3" {
+  bucket        = var.bucket
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_acl" "kotosiro_sharing_s3" {
+  bucket = aws_s3_bucket.kotosiro_sharing_s3.id
+  acl    = var.acl
+}
+
+output "bucket" {
+  value = aws_s3_bucket.kotosiro_sharing_s3.bucket
+}
