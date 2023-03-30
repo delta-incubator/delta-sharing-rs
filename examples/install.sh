8501c5b9eb142046e8f7c0483e2cd0eda62ad919
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2023-03-30 22:00:38 +0900

    This is temporal

--- examples/install.sh
+++ examples/install.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-if pyenv versions | grep -q 3.7.13; then
+if ! pyenv versions | grep -q 3.8.13; then
     pyenv install 3.8.13
 fi
 
-if pyenv versions | grep -q spark3; then
+if ! pyenv versions | grep -q spark3; then
     pyenv virtualenv 3.8.13 spark3
 fi
 
-pyenv shell spark3
+pyenv local spark3
 
 if ! command -v pyspark 1>/dev/null 2>&1 ; then
-    pip install pyspark
+    pip install pyspark delta-spark
 fi
 
 PYSPARK_LOCATION=$(pip show pyspark | grep -e "Location" | cut -d ' ' -f 2)
@@ -19,4 +19,7 @@ PYSPARK_NAME=$(pip show pyspark | grep -e "Name" | cut -d ' ' -f 2)
 export SPARK_HOME="$PYSPARK_LOCATION/$PYSPARK_NAME"
 export PYSPARK_PYTHON=$(pyenv which python)
 
-pyenv local spark3
+#pyspark \
+#    --packages io.delta:delta-core_2.12:2.2.0 \
+#    --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
+#    --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
