84796dac18b0947a08e9e7c2cb3de840c56c31b3
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2023-03-31 15:45:27 +0900

    Add some snippets to test cloud backends

--- sandbox/setup.sh
+++ sandbox/setup.sh
@@ -8,18 +8,10 @@ if ! pyenv versions | grep -q spark3; then
 fi
 
 pyenv local spark3
-
-if ! command -v pyspark 1>/dev/null 2>&1 ; then
-    pip install pyspark delta-spark
-fi
+pip install pyspark delta-spark google-cloud-storage
 
 PYSPARK_LOCATION=$(pip show pyspark | grep -e "Location" | cut -d ' ' -f 2)
 PYSPARK_NAME=$(pip show pyspark | grep -e "Name" | cut -d ' ' -f 2)
 
 export SPARK_HOME="$PYSPARK_LOCATION/$PYSPARK_NAME"
 export PYSPARK_PYTHON=$(pyenv which python)
-
-#pyspark \
-#    --packages io.delta:delta-core_2.12:2.2.0 \
-#    --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
-#    --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
