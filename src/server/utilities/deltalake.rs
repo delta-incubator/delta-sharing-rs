dfccf1c3b35525f3371d9d5709be27fd79a2aa25
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2023-04-04 13:35:01 +0900

    Implement interval

--- src/server/utilities/deltalake.rs
+++ src/server/utilities/deltalake.rs
@@ -3,7 +3,71 @@ use anyhow::Context;
 use anyhow::Result;
 use deltalake::delta::open_table_with_storage_options;
 use deltalake::delta::DeltaTable;
+use std::cmp::max;
+use std::cmp::min;
 use std::collections::hash_map::HashMap;
+use std::fmt;
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub struct Interval<T>
+where
+    T: Copy + PartialOrd + Ord,
+{
+    min: T,
+    max: T,
+}
+
+impl<T> fmt::Display for Interval<T>
+where
+    T: Copy + PartialOrd + Ord + fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Interval: [{}, {}]", self.min, self.max)
+    }
+}
+
+impl<T> fmt::Debug for Interval<T>
+where
+    T: Copy + PartialOrd + Ord + fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Display::fmt(self, f)
+    }
+}
+
+impl<T> Interval<T>
+where
+    T: Copy + PartialOrd + Ord,
+{
+    pub fn new(min: T, max: T) -> Self {
+        Interval { min: min, max: max }
+    }
+
+    pub fn is_before(&self, value: T) -> bool {
+        self.max <= value
+    }
+
+    pub fn contains(&self, value: T) -> bool {
+        self.min <= value && value < self.max
+    }
+
+    pub fn is_after(&self, value: T) -> bool {
+        self.min > value
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.max < self.min
+    }
+
+    pub fn intersect(&self, other: Self) -> Self {
+        let new_min = max(self.min, other.min);
+        let new_max = min(self.max, other.max);
+        Interval {
+            min: new_min,
+            max: max(new_min, new_max),
+        }
+    }
+}
 
 pub struct Utility;
 
@@ -28,5 +92,44 @@ impl Utility {
         )
         .await
         .context("failed to open delta table")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_i64_interval() {
+        let min = testutils::rand::i64(-10, 10);
+        let max = testutils::rand::i64(100, 1000);
+        let interval = Interval::new(min, max);
+        let val = testutils::rand::i64(10, 100);
+        assert!(interval.contains(val));
+        let val = testutils::rand::i64(1000, 2000);
+        assert!(interval.is_before(val));
+        let val = testutils::rand::i64(-100, -10);
+        assert!(interval.is_after(val));
+        let min = testutils::rand::i64(100, 1000);
+        let max = testutils::rand::i64(-10, 10);
+        let interval = Interval::new(min, max);
+        assert!(interval.is_empty());
+    }
+
+    #[test]
+    fn test_string_interval() {
+        let min = testutils::rand::i64(2000, 3000).to_string();
+        let max = testutils::rand::i64(4000, 5000).to_string();
+        let interval = Interval::new(&min, &max);
+        let val = testutils::rand::i64(3000, 4000).to_string();
+        assert!(interval.contains(&val));
+        let val = testutils::rand::i64(5000, 6000).to_string();
+        assert!(interval.is_before(&val));
+        let val = testutils::rand::i64(1000, 2000).to_string();
+        assert!(interval.is_after(&val));
+        let min = testutils::rand::i64(4000, 5000).to_string();
+        let max = testutils::rand::i64(2000, 3000).to_string();
+        let interval = Interval::new(&min, &max);
+        assert!(interval.is_empty());
     }
 }
