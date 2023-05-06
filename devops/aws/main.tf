412c33cddfdcdff8407495bc96c31a40c5c4683d
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2023-05-06 20:57:43 +0900

    Update terraform for AWS

--- devops/aws/main.tf
+++ devops/aws/main.tf
@@ -1,6 +1,7 @@
 variable "access_key" {}
 variable "secret_key" {}
 variable "region" {}
+variable "az" {}
 variable "bucket" {}
 
 provider "aws" {
@@ -9,6 +10,51 @@ provider "aws" {
   region     = var.region
 }
 
+#
+# VPC
+#
+resource "aws_vpc" "kotosiro_sharing_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "kotosiro_sharing_internet_gateway" {
+  vpc_id = aws_vpc.kotosiro_sharing_vpc.id
+}
+
+resource "aws_route" "kotosiro_sharing_to_internet" {
+  route_table_id = aws_vpc.kotosiro_sharing_vpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.kotosiro_sharing_internet_gateway.id
+}
+
+resource "aws_subnet" "kotosiro_sharing_public_subnet" {
+  vpc_id = aws_vpc.kotosiro_sharing_vpc.id
+  availability_zone = var.az
+  cidr_block = "10.0.0.0/18"
+  map_public_ip_on_launch = true
+  depends_on = [aws_internet_gateway.kotosiro_sharing_internet_gateway]
+}
+
+resource "aws_security_group" "allow_http" {
+    name = "allow_http"
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+#
+# S3
+#
 resource "aws_s3_bucket" "kotosiro_sharing_s3" {
   bucket        = var.bucket
   force_destroy = true
