8d8d90dba52b53d8cf16866e0dc6dd3d2cebfc50
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2023-05-15 05:58:30 +0900

    Update terraform files for devops

--- devops/gcp/main.tf
+++ devops/gcp/main.tf
@@ -1,25 +1,37 @@
-variable "project" {}
-variable "bucket" {}
-variable "location" {}
-variable "service_account" {}
+variable "project" {
+  type = string
+}
+
+variable "bucket" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "service_account" {
+  type = string
+}
+
 
 provider "google" {
   project = var.project
 }
 
-resource "google_storage_bucket" "kotosiro_sharing_gcs" {
+resource "google_storage_bucket" "this" {
   name          = var.bucket
   location      = var.location
   force_destroy = true
 }
 
-resource "google_storage_bucket_iam_member" "kotosiro_sharing_gcs" {
-  bucket     = google_storage_bucket.kotosiro_sharing_gcs.name
+resource "google_storage_bucket_iam_member" "this" {
+  bucket     = google_storage_bucket.this.name
   role       = "roles/storage.objectAdmin"
   member     = var.service_account
-  depends_on = [google_storage_bucket.kotosiro_sharing_gcs]
+  depends_on = [google_storage_bucket.this]
 }
 
 output "bucket" {
-  value = google_storage_bucket.kotosiro_sharing_gcs.name
+  value = google_storage_bucket.this.name
 }
