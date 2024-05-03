4e7ceb7dd6d64c7cb456434c82df939d9427b10b
Robert Pack <robstar.pack@gmail.com>
2024-05-03 12:03:44 +0200

    feat: add DiscoveryHandler trait

---
+++ delta-sharing/core/src/error.rs
@@ -0,0 +1,11 @@
+// A convenience type for declaring Results in the Delta Sharing libraries.
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Entity not found.")]
+    NotFound,
+
+    #[error("Recipient is not allowed to read the entity.")]
+    NotAllowed,
+}
